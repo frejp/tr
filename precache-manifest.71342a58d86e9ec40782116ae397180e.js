662629fd7c52d948f80a4940a49178bb4dfee85e
iCodeJavascript <frejcodes@gmail.com>
2020-04-24 13:39:26 +0100

    Updates

---
+++ precache-manifest.71342a58d86e9ec40782116ae397180e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd79d2e69e49e9c84eb9a69fc0fd051",
+    "url": "/tr/index.html"
+  },
+  {
+    "revision": "065aa21fc547e2a62ab2",
+    "url": "/tr/static/css/main.17dd6336.chunk.css"
+  },
+  {
+    "revision": "07e67f65fb75de4ac692",
+    "url": "/tr/static/js/2.0f881b6f.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/tr/static/js/2.0f881b6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065aa21fc547e2a62ab2",
+    "url": "/tr/static/js/main.4bb43031.chunk.js"
+  },
+  {
+    "revision": "030a279e8f4884f90902",
+    "url": "/tr/static/js/runtime-main.4058f226.js"
+  }
+]);
